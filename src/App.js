5e2f112bd0ccfde71c4608fccff123eed8b66dad
Gavin Draper <gavdraper@users.noreply.github.com>
2021-09-11 08:50:25 +0100

    Rename pages/title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand-lg navbar-light">
           <div className="container-fluid">
-            Pace Convert
+            Pace Calculator
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -27,7 +27,7 @@ function App() {
                     to="/Pace"
                     activeClassName="active"
                     className="nav-link"
-                  >Pace Convertor</NavLink>
+                  >Convertor</NavLink>
                 </li>
                 <li className="nav-item">
                   <NavLink
